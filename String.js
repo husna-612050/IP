0940398c3c14340904b2c18589105f6a0a20f904
husna-612050 <109798165+husna-612050@users.noreply.github.com>
2022-11-03 21:27:23 +0530

    Create String.js

---
+++ String.js
@@ -0,0 +1,15 @@
+//printing position
+var str="HUSNA";
+console.log('Character at 2nd position is: ') 
+console.log(str.charAt(2));
+//printing the index
+console.log('Index of H is: ')
+console.log(str.indexOf('H')); 
+//printing the last index of
+console.log('Last index of A is: ')
+console.log(str.lastIndexOf('A'));
+//string slicing
+var mystr = 'my name is husna';
+console.log('The result of String Slicing is: ')
+console.log(mystr.slice(1,6));
+console.log("\nFound 'is' at index "+mystr.search('is'));
