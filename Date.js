b7db658576308cb9fd35d3e928196273bdcefac1
husna-612050 <109798165+husna-612050@users.noreply.github.com>
2022-11-03 21:28:02 +0530

    Create Date.js

---
+++ Date.js
@@ -0,0 +1,18 @@
+//1. printing date
+var date=new Date(); 
+console.log("Today's date: "+date.getDate());
+//2. printing year
+var year=new Date(); 
+console.log("Present Year : "+year.getUTCFullYear()); 
+//3. printing minutes
+console.log('Minutes value: ')
+var min=new Date(); 
+console.log(min.getMinutes()); 
+//4. printing hours
+console.log('Hours value: ')
+var hour=new Date(); 
+console.log(hour.getHours()); 
+//5. printing milliseconds
+console.log('Milliseconds value: ')
+var milli =new Date(); 
+console.log(milli.getMilliseconds());
